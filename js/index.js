ff4f19d04b81df7a37bdf3f070bd4a4dc083ec41
Jada Woody <JadaWoody@ENCMBP01.local>
2020-08-19 17:32:55 -0400

    jadas

--- js/index.js
+++ js/index.js
@@ -21,3 +21,15 @@ else {
 }
 
 // Iteration 3: Loops
+
+console.log(hacker1.split("").join(" ").toUpperCase())
+
+console.log(hacker2.split("").reverse().join(""))
+
+if(hacker1.localeCompare(hacker2) === -1){
+    console.log(`The driver's name goes first`)
+  } else if (hacker1.localeCompare(hacker2) === 1){
+    console.log(`Yo, the navigator goes first definitely.`)
+  } else {
+    console.log(`What?! You both have the same name?`)
+  }
