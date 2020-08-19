152cb19a2573d50d1688e22b0ea5b074fffdd8bb
Ashtyn Ce <ashtyncza@gmaile.com>
2020-08-19 17:03:08 -0400

    help

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 // Iteration 1: Names and Input
 let hacker1 = `Ashtyn`
 
-console.log(`The Driver's name is ${hacker1}`)
+console.log(`The Driver's name is ${hacker1}.`)
+
+let hacker2 = "Rebecca"
+
+console.log(`The Navigator's name is ${hacker2}.`)
 
 
 
