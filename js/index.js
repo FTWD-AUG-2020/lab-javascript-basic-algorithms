413b38e7279c98ac4f0d574108de088aae8fda34
rqsell <68514875+rqsell@users.noreply.github.com>
2020-08-19 17:15:10 -0400

    rebecca's fresh track

--- js/index.js
+++ js/index.js
@@ -10,6 +10,14 @@ console.log(`The Navigator's name is ${hacker2}.`)
 
 
 // Iteration 2: Conditionals
-
+if (hacker1.length> hacker2.length){
+    console.log(`${hacker1}has the longest name it has ${hacker1.length} characters`)
+}
+else if (hacker1.length< hacker2.length) {
+    console.log(`It seems ${hacker2} has the longest name it has ${hacker2.length} characters`)
+}
+else {
+    console.log(`wow you both have equally long names, you both have ${hacker1.length} characters`)
+}
 
 // Iteration 3: Loops
